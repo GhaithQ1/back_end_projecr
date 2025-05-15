ca9355694e8972f76ce7e0fe8d85e916af62d627
GhaithQ1 <ghaithdrh@gmail.com>
2025-05-15 14:50:28 +0300

    ..

--- services/redisClient.js
+++ services/redisClient.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+  url: process.env.REDIS_URL
 });
 
 redisClient.on('error', (err) => {
